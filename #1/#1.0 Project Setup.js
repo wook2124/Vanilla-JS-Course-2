7ac1abb49e77657f43731c6e510abbba2b17d495
wook2124 <wook2124@naver.com>
2020-02-28 19:42:32 +0900

    Initial Commit

--- #1/#1.0 Project Setup.js
+++ #1/#1.0 Project Setup.js
@@ -1,2 +1,20 @@
 // https://github.com/JoyDaheeCha/paintjs 만들기
 // https://joydaheecha.github.io/paintjs/ 
+// index.html 만들기
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="style.css" />
+    <title>PaintJS</title>
+  </head>
+  <body>
+    <script src="app.js"></script>
+  </body>
+</html>
+
+// 터미널(git)을 이용해서 변경사항 바로 저장하기(GitHub)
+git add .
+git commit -m "Initial Commit"
