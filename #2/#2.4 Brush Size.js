f9941c7040c6bcbd6b0c63524296b8f2995bf97f
wook2124 <wook2124@naver.com>
2020-03-03 18:06:17 +0900

    #2.4 Brush Size

--- #2/#2.4 Brush Size.js
+++ #2/#2.4 Brush Size.js
@@ -27,7 +27,6 @@ const mode = document.getElementById("jsMode");
 
 // painting과 동일하게 filling 역시 처음에는 false로 default value(기본값)을 설정해서 painting mode로 시작해줌
 let filling = false;
-
 // click event가 발생해서 handleModeClick function이 기능하고
 // if filling ===(equal) true라면(filling = false 상태인 painting mode이면) mode(button)에 Fill text를 채우고
 // 반대로 filling = true로 filling mode이면 Paint text를 채움
@@ -44,6 +43,81 @@ function handleModeClick() {
 if (mode) {
   mode.addEventListener("click", handleModeClick);
 }
-
 // 추가로 fill, paint 둘 다 소문자로 적어도 대문자로 변형됨
 text-transform: uppercase;
+
+
+// 최종 코드
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+let filling = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function startPainting() {
+  painting = true;
+}
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
+
+function handleRangeChange(event) {
+  const size = event.target.value;
+  ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "Fill";
+  } else {
+    filling = true;
+    mode.innerText = "Paint";
+  }
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
+
+Array.from(colors).forEach(color =>
+  color.addEventListener("click", handleColorClick)
+);
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
+}
