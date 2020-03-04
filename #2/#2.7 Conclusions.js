a8563c5ab882376343e9d9924d130f6d203f38ac
wook2124 <wook2124@naver.com>
2020-03-04 19:50:34 +0900

    #2.7 Conclusions

---
+++ #2/#2.7 Conclusions.js
@@ -0,0 +1,8 @@
+// https://freefmly.github.io/sketch-book/
+// https://eunjung-jenny.github.io/paint_js/
+// https://joydaheecha.github.io/paintjs/
+// 참고해서 이쁘게 꾸미기
+
+
+// https://developer.mozilla.org/ko/docs/Web/HTML/Element
+// HTML 요소 공부하기
